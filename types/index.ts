976455f347ae219145a344a66b5ef6201b6dcdc9
Richard T. Widjaja <rt@richardtirta.com>
2024-07-15 16:34:53 -0700

    Initial commit

---
+++ types/index.ts
@@ -0,0 +1,20 @@
+// types/index.ts
+
+import { PortableTextBlock } from "sanity";
+
+export type ProfileType = {
+  _id: string,
+  fullName: string,
+  headline: string,
+  profileImage: {
+    alt: string,
+    image: string
+  },
+  shortBio: string,
+  email: string,
+  fullBio: PortableTextBlock[],
+  location: string,
+  resumeURL: string,
+  socialLinks: string[],
+  skills: string[],
+};
